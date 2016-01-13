fc94836883ac8e1169355b61928710ed79b8c8f8
Todd Ditchendorf <todd.ditchendorf@gmail.com>
2016-01-13 12:00:38 -0600

    * Initial add

---
+++ ReaderMac/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ReaderMac
+//
+//  Created by Todd Ditchendorf on 1/13/16.
+//  Copyright © 2016 Todd Ditchendorf. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (nonatomic, assign) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
